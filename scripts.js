18d41cd1320ff8848f69c08ce08c867bfd280006
Mandygando <amandavic7576@gmail.com>
2024-02-26 21:28:40 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.write('Hello, world');
+
+console.log('boa noite');
+
+alert('vamos aprender estrutura de dados?');
---
+++ variaveis.js
@@ -0,0 +1,52 @@
+const nome = 'Amanda Vitória';
+const idade= 20;
+const sabeJavaScript = true;
+
+console.log(nome, idade, sabeJavaScript)
+
+
+// exemplo de uso de variáveis em js
+
+var num = 1;
+num = 3;
+var price = 1.5
+var myName = 'Amanda Vitória';
+var trueValue = true;
+var nullVar = null;
+var und;
+
+console.log('num: ' + num);
+console.log('Meu nome: ' + myName)
+console.log('trueValue: ' + trueValue);
+console.log('price: ' + price);
+console.log('nullVar: ' + nullVar);
+console.log('und: ' + und);
+
+
+//operadores
+var num = 0;
+// operadores aritimeticos
+num = num + 2;
+num = num * 3;
+num = num / 2;
+//operadores de atribuição
+num++;
+num--;
+num += 1;
+num -= 2;
+num *- 3;
+num /= 2;
+num % 3;
+
+console.log('num: ' + num);
+
+// operadores de comparação
+console.log('num == 1: ' + (num == 1));
+console.log('num === 1: ' + (num === 1));
+console.log('num != 1: ' + (num != 1));
+console.log('num > 1: ' + (num > 1));
+console.log('num < 1: ' + (num < 1));
+console.log('num >= 1: ' + (num >= 1));
+console.log('true && false: ' + (true && false));
+console.log('true || false: ' + (true || false));
+console.log('!true: ' + (!true));
